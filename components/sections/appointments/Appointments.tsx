eec7a121d630d05a9462f6389a7af092f28f6b89
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-10-05 08:52:51 -0400

    try fix loading of script

--- components/sections/appointments/Appointments.tsx
+++ components/sections/appointments/Appointments.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Section from "../../common/Section";
 import Script from "next/script";
 import { SectionProps } from "../../../types";
@@ -9,9 +9,18 @@ interface Props extends SectionProps {
 }
 
 const Appointments: FC<Props> = ({ _type, title }) => {
+  useEffect(() => {
+    return () => {
+      document.getElementById("appointments")?.remove();
+    };
+  });
+
   return (
     <Section type={_type}>
-      <Script src="https://static.zcal.co/embed/v1/embed.js" />
+      <Script
+        id="appointments"
+        src="https://static.zcal.co/embed/v1/embed.js"
+      />
       <Heading className="text-center">{title}</Heading>
       <div className="zcal-inline-widget">
         <a href="https://zcal.co/i/h6RdOGG5">Massage - Schedule a meeting</a>
