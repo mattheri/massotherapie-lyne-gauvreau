66604e4835b0fec77cbec963083de30bb9089c19
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-10-05 09:02:27 -0400

    try fix loading of script

--- components/sections/appointments/Appointments.tsx
+++ components/sections/appointments/Appointments.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import Section from "../../common/Section";
 import Script from "next/script";
 import { SectionProps } from "../../../types";
@@ -9,19 +9,30 @@ interface Props extends SectionProps {
 }
 
 const Appointments: FC<Props> = ({ _type, title }) => {
+  const [addScript, setAddScript] = useState(false);
+
   useEffect(() => {
+    const zCalScript = document.getElementById("zCalScript");
+    const container = document.getElementById("appointments-container");
+
+    if (!container?.contains(zCalScript)) {
+      setAddScript(true);
+    }
     return () => {
-      document.getElementById("appointments")?.remove();
+      setAddScript(false);
+      zCalScript?.remove();
     };
   });
 
   return (
-    <Section type={_type}>
-      <Script
-        id="appointments"
-        src="https://static.zcal.co/embed/v1/embed.js"
-      />
+    <Section type={_type} id="appointments-container">
       <Heading className="text-center">{title}</Heading>
+      {addScript && (
+        <Script
+          id="zCalScript"
+          src="https://static.zcal.co/embed/v1/embed.js"
+        />
+      )}
       <div className="zcal-inline-widget">
         <a href="https://zcal.co/i/h6RdOGG5">Massage - Schedule a meeting</a>
       </div>
