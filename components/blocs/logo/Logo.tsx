403ca69468714025390a8e71abe73599b63b9528
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-10-07 12:05:33 -0400

    Added logo

--- components/blocs/logo/Logo.tsx
+++ components/blocs/logo/Logo.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import SVG from 'react-inlinesvg'
+import React, { FC } from "react";
+import SVG from "react-inlinesvg";
+import { Asset } from "../../../types";
 import styles from "./Logo.module.scss";
 
-const Logo = ({ logo }) => {
-	if (!logo || !logo.asset) {
-		return null
-	}
-
-	if (logo.asset.extension === 'svg') {
-		return <SVG src={logo.asset.url} className={styles.root} />
-	}
-
-	return <img src={logo.asset.url} alt={logo.title} className={styles.root} />
+interface Props {
+  logo?: Asset;
 }
 
-export default Logo
+const Logo: FC<Props> = ({ logo }) => {
+  if (!logo || !logo.asset) {
+    return null;
+  }
+
+  if (logo.asset.extension === "svg") {
+    return <SVG src={logo.asset.url} className={styles.root} />;
+  }
+
+  return <img src={logo.asset.url} alt={logo.title} className={styles.root} />;
+};
+
+export default Logo;
