5380f3f5a8f51f489840480d882165a346ef8ddf
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-10-19 12:10:53 -0400

    now request location onclick

--- components/sections/map/Map.tsx
+++ components/sections/map/Map.tsx
@@ -1,11 +1,10 @@
 import type { SectionComponent } from "../../../types";
 
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 
 import { Section } from "../../common";
 
 import Container from "react-bootstrap/Container";
-import Link from "next/link";
 
 import useLocation from "../../../hooks/useLocation";
 import useInitMap from "../../../hooks/useInitMap";
@@ -14,20 +13,18 @@ import styles from "./Map.module.scss";
 
 const Map: SectionComponent = ({ _type, revealInViewport, location }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [href, setHref] = useState("#");
 
   useInitMap(location, ref);
 
-  const coords = useLocation();
+  const { coords, requestLocation } = useLocation();
 
   useEffect(() => {
     if (!coords) return;
 
     const { lat, lng } = coords;
+    const href = `https://www.google.com/maps/dir/${lat},${lng}/${location.lat},${location.lng}`;
 
-    setHref(
-      `https://www.google.com/maps/dir/${lat},${lng}/${location.lat},${location.lng}`
-    );
+    window.open(href, "_blank", "noopener,noreferrer");
   }, [coords, location]);
 
   return (
@@ -37,11 +34,9 @@ const Map: SectionComponent = ({ _type, revealInViewport, location }) => {
       className={styles.root}
       root={{ className: styles.container }}
     >
-      <Link href={href}>
-        <a className={styles.link} target="_blank" rel="noopener noreferrer">
-          <Container className="px-0 h-100" fluid ref={ref}></Container>
-        </a>
-      </Link>
+      <article onClick={requestLocation} className={styles.link}>
+        <Container className="px-0 h-100" fluid ref={ref}></Container>
+      </article>
     </Section>
   );
 };
--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -1,39 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 type Coords = { lat: number; lng: number };
 
 const useLocation = () => {
   const [coords, setCoords] = useState<Coords | undefined>(undefined);
 
-  const requestLocation: () => Promise<
-    GeolocationPosition | PositionErrorCallback
-  > = () => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+  const dispatchLocationEvent = (geo: GeolocationPosition) => {
+    const { latitude, longitude } = geo.coords;
+    const coords = { lat: latitude, lng: longitude };
+    setCoords(coords);
   };
 
-  const getCoords = async () => {
-    const position = await requestLocation();
+  const dispatchLocationErrorEvent = (error: GeolocationPositionError) => {
+    throw new Error(error.message);
+  };
 
-    if (position instanceof Error) {
-      throw position;
-    }
-
-    if ("coords" in position) {
-      const coords = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-      return coords;
+  const requestLocation = async () => {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          dispatchLocationEvent,
+          dispatchLocationErrorEvent
+        );
+      } else {
+        throw new Error("Geolocation is not supported by this browser.");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  useEffect(() => {
-    getCoords().then(setCoords);
-  }, []);
-
-  return coords;
+  return {
+    coords,
+    requestLocation,
+  };
 };
 
 export default useLocation;
